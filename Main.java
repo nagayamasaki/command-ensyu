d13c301d770537cca1d8e3291a71eeb86cbd69fc
nagayama <s.nagayama.sys22@morijyobi.ac.jp>
2022-11-10 13:31:14 +0900

    2人分計算できるように修正

--- Main.java
+++ Main.java
@@ -4,13 +4,21 @@ public class Main {
         public static void main(String[] args) {
                 Scanner stdIn = new Scanner(System.in);
 
-                System.out.print("身長(cm):");
-                double height = stdIn.nextDouble();
-                System.out.print("体重(kg):");
-                double weight = stdIn.nextDouble();
-				double h = height / 100;
-                double bmi = weight / h / h;
+ 		System.out.print("1人目の身長(cm):");
+                double height1 = stdIn.nextDouble();
+                System.out.print("1人目の体重(kg):");
+                double weight1 = stdIn.nextDouble();
+		double h1 = height1 / 100;
+                double bmi1 = weight1 / h1 / h1;
+        	
+        	System.out.print("2人目の身長(cm):");
+                double height2 = stdIn.nextDouble();
+                System.out.print("2人目の体重(kg):");
+                double weight2 = stdIn.nextDouble();
+		double h2 = height2 / 100;
+                double bmi2 = weight2 / h2 / h2;
 
-                System.out.println("BMI:"+ (Math.floor(bmi * 100)) / 100);
+                System.out.println("1人目のBMI:"+ (Math.floor(bmi1 * 100)) / 100);
+	        System.out.println("2人目のBMI:"+ (Math.floor(bmi2 * 100)) / 100);
         }
 }
